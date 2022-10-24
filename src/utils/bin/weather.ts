1e76713c885b9f7dc4804e205e60079a08756ea4
abraham-yusuf <1703020028@students.unis.ac.id>
2022-10-24 21:09:18 +0000

    add city

--- src/utils/bin/weather.ts
+++ src/utils/bin/weather.ts
@@ -4,7 +4,7 @@ export const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
 
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Usage: weather [city]. Example: weather Jakarta';
   }
 
   const weather = await getWeather(city);
